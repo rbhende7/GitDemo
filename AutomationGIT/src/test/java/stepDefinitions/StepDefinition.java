8fd4f7a8017526bf69c5056419a6439c9fbfa75a
rbende <rbende7@gmail.com>
2020-04-26 20:17:47 -0400

    second commit

--- AutomationGIT/src/test/java/stepDefinitions/StepDefinition.java
+++ AutomationGIT/src/test/java/stepDefinitions/StepDefinition.java
@@ -9,26 +9,26 @@ public class StepDefinition {
 	@Given("^Initialize the browser with chrome$")
 	public void initialize_the_browser_with_chrome() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("STARTING TEST WITH MAVEN-GIT");
+		System.out.println("STARTING TEST WITH JENKINS-GIT");
 	    
 	}
 
 	@Given("^Navigate to \"([^\"]*)\" site$")
 	public void navigate_to_site(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("STARTING TEST WITH MAVEN-GIT");
+		System.out.println("STARTING TEST WITH JENKINS-GIT");
 	    
 	}
 
 	@Given("^Click on Login link in home page to land on Secure sign in Page$")
 	public void click_on_Login_link_in_home_page_to_land_on_Secure_sign_in_Page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("GIT-Click on Login link in home page to land on Secure sign in Page");
+		System.out.println("JENKINS-GIT-Click on Login link in home page to land on Secure sign in Page");
 	}
 
 	@When("^User enters (.+) and (.+) and logs in$")
     public void user_enters_and_and_logs_in(String username, String password) throws Throwable {
-		System.out.println("GIT-username=" + username + " password=" + password);
+		System.out.println("JENKINS-GIT-username=" + username + " password=" + password);
         
     }
 
@@ -36,7 +36,7 @@ public class StepDefinition {
 	@Then("^Verify that user is successfully logged in$")
 	public void verify_that_user_is_successfully_logged_in() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("GIT-Verify that user is successfully logged in");
+		System.out.println("JENKINS-GIT-Verify that user is successfully logged in");
 	}
 
 }
